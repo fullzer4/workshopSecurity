b372356c922c69e54a0900c85ce5494cc91210b1
Gabriel Pelizzaro Pereira <gabrielpelizzaro@gmail.com>
2023-05-21 21:32:41 +0000

    Delete rust_codes/ex2/target directory

--- rust_codes/ex2/target/debug/deps/ex2-7c2d202ef7a658be.d
+++
@@ -1,5 +0,0 @@
-/run/media/fullzer4/8b61b96a-ee7c-4a69-99e4-41120d4337b0/workshop/workshopSecurity/rust_codes/ex2/target/debug/deps/ex2-7c2d202ef7a658be: src/main.rs
-
-/run/media/fullzer4/8b61b96a-ee7c-4a69-99e4-41120d4337b0/workshop/workshopSecurity/rust_codes/ex2/target/debug/deps/ex2-7c2d202ef7a658be.d: src/main.rs
-
-src/main.rs:
